3012e52b91ca594c4461b855c80fd895c20fea2b
Hugy0707 <99295735+Hugy0707@users.noreply.github.com>
2025-11-27 21:48:57 -0300

    html da api

--- scriptDOis.js
+++ scriptDOis.js
@@ -1,19 +1,38 @@
-async function buscarEndereco(){
-    const cep = document.getElementById('cep').value;
+async function buscarendereco() {
 
-    if(!cep){
-        alert('Digitar um CEP válido');
-    }
+    const Link_API = 'https://humble-space-enigma-xgvgpg9grw7f9954-8080.app.github.dev/';
 
-    try{
-        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        const dados = await resposta.json(); //converter a resposta em JSON
+    const pd_form = document.getElementById('pd_form');
+    const pd_resultado = document.getElementById('pd_resultado');
 
-        document.getElementById('dados_logradouro').textContent = dados.logradouro;
-        document.getElementById('dados_logradouro').textContent = dados.logradouro;
-    }
-    catch(err){
-        console.log('Erro', err);
-        document.getElementById('dados').textContent = 'Erro ao buscar o CEP \nTente novamente mais tarde.';
-    }
+    pd_form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const dados = {
+            nome: pd_form.nome.value,
+            categoria: pd_form.categoria.value,
+            descricao: pd_form.descricao.value,
+            disponivel: pd_form.disponivel.checked,
+            preco: parseFloat(pd_form.preco.value) ,
+        };
+
+        try {
+            const resposta = await fetch(`${Link_API}api/lanchonete`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(dados)
+            });
+
+
+            const resultado = await resposta.json();
+
+            pd_resultado.textContent= JSON.stringify(resultado,null,2)
+            //pd_resultado.textContent= "Cadastradp com sucesso";
+
+        } catch (err) {
+            console.log('Erro:', err);
+            pd_resultado.textContent = "Erro ao enviar dados para API"
+        }
+    });
 }
+buscarendereco();
